3bdbf06916858974639ea5d5c14cabb0856d57bd
hyteer <hyteer@qq.com>
2015-12-19 00:17:19 +0800

    Use handlebars template engine in project.

--- index.js
+++ index.js
@@ -2,32 +2,46 @@ var express = require('express')
 
 var app = express();
 
+//set up handlebars view engine
+var exphbs = require('express-handlebars');
+var handlebars = exphbs.create({
+	defaultLayout: 'main',
+	extname: '.hbs',
+	helpers: {
+		foo: function() {return 'Foo!';},
+		bar: function() {return 'Bar!';}
+	}
+});
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
+
 app.set('port', process.env.PORT || 3000);
 
 //Routes for Home and About pages
 app.get('/', function(req, res){
-	res.type('text/plain');
-	res.send('Meadowlark Travel')
+	res.render('home');
 });
 app.get('/about', function(req, res){
+	res.render('about')
+});
+
+app.get('/about/yt', function(req, res){
 	res.type('text/plain');
-	res.send('About Meadowlark Travel');
+	res.send('About YT...');
 });
 
 
 //custom 404 page
 app.use(function(req, res){
-	res.type('text/plain');
 	res.status(404);
-	res.send('404 - Not Found');
+	res.render('404');
 });
 
 //custom 500 page
 app.use(function(req, res){
 	console.error(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
+	res.render(500);
 });
 
 app.listen(app.get('port'), function(){
--- readme.txt
+++ readme.txt
@@ -1,1 +1,1 @@
-This is a simulated web site for leanring nodejs.
+This is a simulated web site for leanring Nodejs,Express and MongoDB.

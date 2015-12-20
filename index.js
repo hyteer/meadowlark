d4c23e7929afa914634896db3d2bd653b20787b6
hyteer <hyteer@qq.com>
2015-12-20 21:53:21 +0800

    Chapter7-Handlebars partials

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ var fortune = require('./lib/fortune.js');
 //Add dir 'public' as static resource
 app.use(express.static(__dirname + '/public'));
 
-// Fortunes for user's cookie
+// Disable Express x-powered-by
+app.use(function(req, res, next){
+	res.removeHeader("X-Powered-By");
+	next();
+});
 
 
 //set up handlebars view engine
@@ -21,7 +25,46 @@ var handlebars = exphbs.create({
 app.engine('.hbs', handlebars.engine);
 app.set('view engine', '.hbs');
 
+
 app.set('port', process.env.PORT || 3000);
+
+
+// mocked weather data
+function getWeatherData(){
+	return {
+		locations: [
+			{
+				name: 'Portland',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+				weather: 'Overcast',
+				temp: '54.1 F (12.3 C)',
+			},
+			{
+				name: 'Bend',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+				weather: 'Partly Cloudy',
+				temp: '55.0 F (12.8 C)',
+			},
+			{
+				name: 'Manzanita',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+				weather: 'Light Rain',
+				temp: '55.0 F (12.8 C)',
+			},
+		],
+	};
+}
+
+// middleware to add weather data to context
+app.use(function(req, res, next){
+	if(!res.locals.partials) res.locals.partials = {};
+	res.locals.partials.weatherContext = getWeatherData();
+	next();
+});
+
 
 //Routes for Home and About pages
 app.get('/', function(req, res){
@@ -31,10 +74,28 @@ app.get('/about', function(req, res){
 	res.render('about', { fortune: fortune.getFortune() });
 });
 
+//YT test.
 app.get('/about/yt', function(req, res){
 	res.type('text/plain');
 	res.send('About YT...');
 });
+
+//Render a plain text for test page
+app.get('/test', function(req, res){
+	res.type('text/plain');
+	res.send('this is a test');
+});
+
+//Pages have no layout file
+app.get('/nolayout', function(req, res){
+	res.render('nolayout', { layout: null });
+});
+
+//custom layout
+app.get('/customtest',function(req, res){
+	res.render('customtest', { layout: 'custom'})
+})
+
 
 //custom 404 page
 app.use(function(req, res){
@@ -48,6 +109,11 @@ app.use(function(req, res){
 	res.status(500);
 	res.render(500);
 });
+
+
+
+
+
 
 
 
